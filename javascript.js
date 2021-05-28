77a9f1d2054136acdc134d97ae139e24fa7f779c
alzahrawais <magaries1728@gmail.com>
2021-05-28 21:25:37 +0500

    update

--- javascript.js
+++ javascript.js
@@ -36,3 +36,37 @@
 // 	}
 // };
 // console.log( computer.name);
+// function addition(x,y){
+// 	setTimeout(function()
+// 		{console.log(x+y),result()},
+// 		3000);
+	
+// }
+// addition(4,5);
+
+// function result(){
+// 	console.log("done!");
+// }
+const express()=require('express')
+const app = express()
+
+app.listen(1212,()=>{
+	console.log("your port is 1212");
+})\
+// var http = require("http");
+// http.createServer(function (request, response) {
+// // Send the HTTP header
+// // HTTP Status: 200 : OK
+// // Content Type: text/plain
+// response.writeHead(200, {'Content-Type': 'text/plain'});
+// // Send the response body as "Hello World"
+// response.end('Hello World\n');
+// }).listen(8081);
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/')
+// var http = require('http');
+
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hello World!');
+// }).listen(8080);
